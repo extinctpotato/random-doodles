88dae452c82a4430ed959b979ecb2bd18c1aa2e3
Adam Olech <nddr89@gmail.com>
2022-04-22 01:02:33 +0200

    Add some cool sizeof tests

--- test.c
+++ test.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <linux/time.h>
 
 int main(int argc, char **argv) {
 	printf("Hello world %d\n", argc);
@@ -7,4 +7,10 @@ int main(int argc, char **argv) {
 	for (int i=0; i < argc; i++) {
 		printf("argv[%d]: %s\n", i, argv[i]);
 	}
+
+	printf("%lu, %lu, %lu\n", 
+			sizeof(long long int), 
+			sizeof(int), 
+			sizeof(struct timeval)
+			);
 }
