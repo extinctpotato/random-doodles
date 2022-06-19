72ea5b7c4d44dc3d3b2f1d94d08284f5b5489433
Adam Olech <nddr89@gmail.com>
2022-06-19 23:38:42 +0200

    get_mtime.sh: change escape char

--- get_mtime.sh
+++ get_mtime.sh
@@ -6,4 +6,4 @@
 # within the path provided as first positional argument. #
 ##########################################################
 
-find $1 -print0 | xargs -0 stat --printf='%n\t@%Y\n' | sed "s/$1\///g"
+find $1 -print0 | xargs -0 stat --printf='%n\t@%Y\n' | sed "s#$1\/##g"
